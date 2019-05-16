c545eb70c798ceaf76557bf81a20e9094970202e
Gabriel Splendiani <gabrielsdi18@gmail.com>
2019-05-16 18:29:07 -0300

    add query,item and item description services

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,45 @@
 var express = require('express');
+var request = require('request');
 var app = express();
+var queryUrl = 'https://api.mercadolibre.com/sites/MLA/search?q=:';
+var itemUrl = 'https://api.mercadolibre.com/items/';
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function (req, res) {  
+});
+
+app.get('/api/query/:query', function (req, res) {
+    var query = req.params.query;
+    var url = queryUrl + query;
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var info = JSON.parse(body)          
+          res.send(info);
+        }
+      })
+});
+
+app.get('/api/item/:id', function (req, res) {
+    var id = req.params.id;    
+    var url = itemUrl + id;
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var info = JSON.parse(body)          
+          res.send(info);
+        }
+      })
+});
+
+app.get('/api/item/:id/description', function (req, res) {
+    var id = req.params.id;    
+    var url = itemUrl + id + "/description";
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var info = JSON.parse(body)          
+          res.send(info);
+        }
+      })
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('listening on port 3000!');
 });
