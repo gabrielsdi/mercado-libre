8154006b57e682bc2893ae8c59c1916a979c5acf
Gabriel Splendiani <gabrielsdi18@gmail.com>
2019-05-21 23:02:37 -0300

    old project deleted

--- frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import Routes from './routes/routes';
-
-
-ReactDOM.render(<Routes />, document.getElementById('root'));
-serviceWorker.unregister();
